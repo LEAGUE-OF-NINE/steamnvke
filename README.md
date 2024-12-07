19d8d57d21e631f88b18389037f9e6895895f33a
pufferffish <me@windtfw.com>
2024-12-07 08:07:12 +0000

    add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# steamnvke
+
+steamnvke is a Rust library for removing DRM of the SteamStub variants.
+This is essentially a Rust reimplementation for [steamless](https://github.com/atom0s/Steamless) in case you
+need to a Rust library to remove SteamStub DRM for whatever reasons.
+
+# Thanks
+Thanks to atom0s for [steamless](https://github.com/atom0s/Steamless) which this project is based on.
--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,15 @@
-use std::fs;
+use std::{env, fs};
 use pelite::FileMap;
 
 fn main() {
-    let path = "/Users/octeep/Downloads/LimbusCompany.exe";
-    if let Ok(map) = FileMap::open(path) {
-        let new_file = steamnvke::drm::strip_drm_from_exe(map.as_ref()).unwrap();
-        println!("Written {} bytes", new_file.len());
-        fs::write("unpacked.exe", new_file).unwrap();
-    }
+    let args: Vec<String> = env::args().collect();
+    let path = match args.get(1) {
+        None => panic!("No path supplied in arguments"),
+        Some(p) => p,
+    };
+
+    let map = FileMap::open(path).unwrap();
+    let new_file = steamnvke::drm::strip_drm_from_exe(map.as_ref()).unwrap();
+    println!("Written {} bytes", new_file.len());
+    fs::write("unpacked.exe", new_file).unwrap();
 }
